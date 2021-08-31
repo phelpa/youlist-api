ca795cc6a1d329d54d8d700702d500b9392f771e
phelpa <pliniohelpa@gmail.com>
2021-08-30 21:42:49 -0300

    feature: add protocols, helpers and errors

---
+++ src/presentation/helpers/http/http-helper.ts
@@ -0,0 +1,37 @@
+import { HttpResponse } from 'presentation/protocols/http'
+import { ServerError, UnauthorizedError } from 'presentation/errors'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const validationError = (reason: object): HttpResponse => ({
+  statusCode: 422,
+  body: { "message" : reason }
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
