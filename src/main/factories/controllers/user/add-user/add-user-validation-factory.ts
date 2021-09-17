aa73db1cc4bf5f0f1389bf408323bc267235a7b4
phelpa <pliniohelpa@gmail.com>
2021-09-17 09:54:38 -0300

    refactor: refactor adduser validation

--- src/main/factories/controllers/user/add-user/add-user-validation-factory.ts
+++ src/main/factories/controllers/user/add-user/add-user-validation-factory.ts
@@ -1,6 +1,6 @@
 import { Validation } from 'presentation/protocols/validation'
-import { BaseYupValidator } from 'validation/validators/base-yup-validator'
-import { YupAddUserSchema } from 'validation/validators/user/yup-add-user-schema'
+import { BaseYupValidator } from 'validation/base-yup-validator'
+import { YupAddUserSchema } from 'validation/yupschemas/user/yup-add-user-schema'
 
 export const makeAddUserValidation = (): Validation => {
   const addUserSchema = YupAddUserSchema()
