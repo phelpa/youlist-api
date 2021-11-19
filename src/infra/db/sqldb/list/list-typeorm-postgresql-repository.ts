67def1ecae09d7a95fc74d46d9311188e34f7827
phelpa <pliniohelpa@gmail.com>
2021-11-18 21:40:45 -0300

    feat: add video when creating list

--- src/infra/db/sqldb/list/list-typeorm-postgresql-repository.ts
+++ src/infra/db/sqldb/list/list-typeorm-postgresql-repository.ts
@@ -1,20 +1,37 @@
 import { ListHelper } from '../helpers/list-helper'
+import { VideoHelper } from '../helpers/video-helper'
 import { AddListRepository } from 'data/protocols/db/list/add-list-repository'
 import { GetListsRepository } from 'data/protocols/db/list/get-lists-repository'
+import { GetVideoYoutubeInfoApi } from 'data/protocols/db/video/get-video-youtube-info-api'
 import { ListModel } from 'domain/models/list'
 import { addListParams } from 'domain/usecases/list/add-list'
 
-export class ListTypeOrmPostgreSqlRepository implements AddListRepository, GetListsRepository {
-  async add (data: addListParams): Promise<ListModel> {
+export class ListTypeOrmPostgreSqlRepository
+  implements AddListRepository, GetListsRepository
+{
+  constructor(private readonly youtubeApi: GetVideoYoutubeInfoApi) {}
+  async add(data: addListParams): Promise<ListModel> {
     const list = ListHelper.newList(data)
     const insertedList = await list.save()
+    const ytVideoInfo = await this.youtubeApi.getVideoInfo(data.youtube_id)
+
+    const shortenedTitle = ytVideoInfo.title.slice(0, 100)
+    const shortenedDescription = ytVideoInfo.description.slice(0, 254)
+
+    const firstVideo = VideoHelper.newVideo({
+      title: shortenedTitle,
+      description: shortenedDescription,
+      youtube_id: ytVideoInfo.youtube_id,
+      list_id: insertedList.lst_id
+    })
+    await firstVideo.save()
+
     const mappedList = ListHelper.mapper(insertedList)
     return mappedList
   }
 
-  async get (params: Partial<ListModel>): Promise<ListModel[]> {
+  async get(params: Partial<ListModel>): Promise<ListModel[]> {
     const lists = await ListHelper.find(params)
     return lists
   }
-
 }
--- src/main/factories/usecases/list/add-list/db-add-list-factory.ts
+++ src/main/factories/usecases/list/add-list/db-add-list-factory.ts
@@ -1,8 +1,15 @@
 import { AddList } from 'domain/usecases/list/add-list'
 import { DbAddList } from 'data/usecases/list/add-list/db-add-list'
 import { ListTypeOrmPostgreSqlRepository } from 'infra/db/sqldb/list/list-typeorm-postgresql-repository'
+import { YoutubeApi } from 'infra/youtube/video/youtube-api'
+import { AxiosHttpClient } from 'infra/httpClient/axios-client'
+import env from 'main/config/env'
 
 export const makeDbAddList = (): AddList => {
-  const listTypeOrmPostGreSqlRepository = new ListTypeOrmPostgreSqlRepository()
+  const httpGetClient = new AxiosHttpClient()
+  const youtubeApi = new YoutubeApi(httpGetClient, env.youtubeKey)
+  const listTypeOrmPostGreSqlRepository = new ListTypeOrmPostgreSqlRepository(
+    youtubeApi
+  )
   return new DbAddList(listTypeOrmPostGreSqlRepository)
 }
